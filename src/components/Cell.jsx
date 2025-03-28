3162d7653414979af440419c069f6582ab70334a
unexCoder <unexCoder@Micaelas-MacBook-Pro.local>
2025-03-28 11:07:03 -0300

    2025 first update

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -12,16 +12,38 @@ const Cell = (props) => {
     // console.log(size);
     const numCells = Math.round((size.width/cellW)*(size.height/cellW));
     const orientation = size.width > size.height ? true : false;
-    const linkPos = orientation ? [0.135,0.285,0.43,0.575] : [0.135,0.39,0.6,0.86];
+    const linkPos = orientation ? [0.115,0.285,0.42,0.627] : [0.135,0.39,0.6,0.86];
     const tube = 'https://www.youtube.com/channel/UClEoTMrqLGcBSXn6Jl5KB6A';
     const bandcamp = 'https://unexcoder.bandcamp.com/';
     const github = 'https://github.com/unexCoder';
 
+    // Set random values to each cell an load page
+    useEffect(() => {
+        if(numCells > 0) {
+            // Initialize all cells with random characters
+            for(let i = 0; i < numCells; i++) {
+                const cell = document.getElementById('id'+i);
+                if(cell) {
+                    cell.innerHTML = randomChar();
+                    // Randomly set some cells to have different styling
+                    if(Math.random() > 0.8) {
+                        cell.style.opacity = '1.0';
+                        cell.style.color = 'green';
+                    }
+                }
+            }
+        }
+    }, [numCells]); // Only run when numCells changes
+    
+    // Set static cells with links
     useEffect(() => {
         if(numCells > 0) {
             staticCharCell(Math.floor(numCells*linkPos[0]),'\\unexCoder','/home'); 
             staticCharCell(Math.floor(numCells*linkPos[1]),'albums_',bandcamp);
+            // staticCharCell(Math.floor(numCells*linkPos[1]-1),'albums_',bandcamp);
             staticCharCell(Math.floor(numCells*linkPos[2]),'a/visuals','hypercubx'); 
+            // staticCharCell(Math.floor(numCells*linkPos[2]-2),'a/visuals','hypercubx'); 
+            // staticCharCell(Math.floor(numCells*linkPos[2]+3),'a/visuals','hypercubx'); 
             staticCharCell(Math.floor(numCells*linkPos[3]),'*code',github); 
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
