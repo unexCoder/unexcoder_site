fcaa3c4646c09ebffbb6de0ab69f2e28b49c4f75
unexCoder <unexCoder@unexCoder.local>
2026-01-16 19:35:03 -0300

    upgrade to react 19  andfix netlify deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import { HelmetProvider } from 'react-helmet-async';
 import './i18n'
 
-ReactDOM.render(
+// ✅ Sintaxis nueva (React 19)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
